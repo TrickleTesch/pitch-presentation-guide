fa124a39c4842838bec4ba7bab171bf89645d7c8
TrickleTesch <erik.tesch@trickle.se>
2025-03-21 13:31:23 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Pitch Presentation Guide</h1>
+            <p>Your guide to presenting a Pitch deck smoothly.</p>
+        </div>
+    );
+}
